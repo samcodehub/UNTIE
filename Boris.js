48dd20f9a9754b133105797b2d869f5a0e8f5056
Sam CodeHub <101159337+samcodehub@users.noreply.github.com>
2023-05-10 16:49:46 -0300

    Add files via upload

---
+++ Boris.js
@@ -0,0 +1,19 @@
+const upArrow = document.getElementById("up");
+const downArrow = document.getElementById("down");
+const overlay = document.querySelector(".overlay");
+
+upArrow.addEventListener("click", function() {
+ 
+});
+
+downArrow.addEventListener("click", function() {
+  
+});
+
+
+
+
+
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image);
+console.log("SimpleParallax library loaded successfully");
